72f75ad2eb4dfba999d8c8180f04fe628863d371
BEN <bchaddon@gmail.com>
2023-08-07 21:20:35 -0700

    fixed gui behavior to reset pickwindow correctly

--- gui.py
+++ gui.py
@@ -11,15 +11,21 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 400, 400)
         vbox = QVBoxLayout()
 
-        # Add button for each .csv in the cubes folder
+        ## update bulk json
+        if not os.path.exists('bulk-data.json'):
+            oracle.write_bulk_json()
+        else:
+            oracle.update_bulk_json()
+
+        ## add button for each .csv in the cubes folder
         self.csvs = [csv for csv in os.listdir('cubes') if csv.endswith('.csv')]
         for csv in self.csvs:
-            cube_name = csv[:-4]  # Remove the extension (.csv)
+            cube_name = csv[:-4]  ## remove the extension (.csv)
             csv_button = QPushButton(cube_name, self)
             csv_button.clicked.connect(lambda _, cube_name=cube_name: self.button_clicked(cube_name))
             vbox.addWidget(csv_button)
 
-        # Add quit button
+        ## add quit button
         quit_button = QPushButton('Quit', self)
         quit_button.clicked.connect(self.close)
         vbox.addWidget(quit_button)
@@ -30,12 +36,15 @@ class MainWindow(QMainWindow):
 
     def button_clicked(self, cube_name):
         self.pick_window = PickWindow(cube_name=cube_name)
-        self.pick_window.closed.connect(self.show)
+        self.pick_window.pick_completed.connect(self.handle_pick_completed)
         self.hide()
         self.pick_window.show()
+    
+    def handle_pick_completed(self):
+        self.show()
 
 class PickWindow(QWidget):
-    closed = pyqtSignal()
+    pick_completed = pyqtSignal()
 
     def __init__(self, cube_name):
         super().__init__()
@@ -45,8 +54,18 @@ class PickWindow(QWidget):
     def init_ui(self):
         self.setWindowTitle(f'{self.cube_name} - select p1p1')
         self.setGeometry(500, 100, 400, 400)
+
+        if self.layout():
+            layout = self.layout()
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget:
+                    widget.setParent(None)
+
         grid = QGridLayout()
 
+        ## establish engine, create tables (if not exist), create cube_list list object
         self.engine = db.create_engine(f'sqlite:///databases//{self.cube_name}.db')
         db.create_tables(self.engine)
 
@@ -55,19 +74,14 @@ class PickWindow(QWidget):
 
         self.cube_list = db.to_list_from_(self.list_df)
 
-        ## update bulk json
-        if not os.path.exists('bulk-data.json'):
-            oracle.write_bulk_json()
-        else:
-            oracle.update_bulk_json()
-
         ## create cube card info json
-        oracle.extract_oracle_json()
+        oracle_uri = oracle.get_oracle_uri()
+        oracle.update_oracle_json(oracle_uri)
         filtered_json = oracle.filter_oracle_json_for_(self.cube_list)
-        extracted_json = oracle.extract_info_from_(filtered_json)
+        extracted_info_json = oracle.extract_info_from_(filtered_json)
 
         ## add cube card info to info table
-        json_df = db.json_df_from_(extracted_json)
+        json_df = db.json_df_from_(extracted_info_json)
         db.add_info_to_(self.engine, json_df)
 
         ## add 15 buttons in a 3x5 grid
@@ -79,25 +93,23 @@ class PickWindow(QWidget):
 
         ## add exit button
         exit_button = QPushButton('Exit', self)
-        exit_button.clicked.connect(self.close_pick_window)
-        grid.addWidget(exit_button, 3, 2, 1, 1)  # Span the button across 5 columns
+        exit_button.clicked.connect(self.handle_exit_button_click)
+        grid.addWidget(exit_button, 3, 2, 1, 1)  ## span the button across 5 columns
 
         self.setLayout(grid)
 
+    def handle_exit_button_click(self):
+        self.pick_completed.emit()
+        self.close()
+
     def button_clicked(self, card):
-        if card == 'Exit':
-            self.closed.emit()
-        else:
-            # Pull picked card to the first index position, then add to db
+        if card != 'Exit':
+            ## pull picked card to the first index position, then add to db
             self.current_pack.insert(0, self.current_pack.pop(self.current_pack.index(card)))
             p1p1 = db.create_picks_object(self.current_pack)
             db.add_pick_to_(self.engine, p1p1)
-            # Make a new Pick Window instance
-            new_pick_window = PickWindow(self.cube_name)
-            new_pick_window.closed.connect(self.show)
-            self.close()
-            new_pick_window.show()
-
-    def close_pick_window(self):
-        self.closed.emit()
-        self.close()
+            
+        ## clear widgets from current layout and reload
+        for i in reversed(range(self.layout().count())):
+            self.layout().itemAt(i).widget().setParent(None)
+        self.init_ui()
--- oracle.py
+++ oracle.py
@@ -21,25 +21,24 @@ def update_bulk_json() -> None:
     res_dt = datetime.fromisoformat(res_json['updated_at'])
     last_updated_dt = datetime.fromtimestamp(path.getmtime('bulk-data.json'),pytz.UTC)
     if res_dt > last_updated_dt:
-        new_json_uri = res_json['download_uri']
-        new_json_res = requests.get(new_json_uri)
-        new_json_res.raise_for_status()
         with open('bulk-data.json','wb') as file:
-            file.write(new_json_res.content)
+            file.write(bulk_res.content)
 
-def extract_oracle_json() -> None:
+def get_oracle_uri() -> str:
     with open('bulk-data.json','r') as bulk_json:
-        loaded_bulk_json = json.load(bulk_json, encoding='UTF-8')
-        uri = loaded_bulk_json['download_uri']
-        oracle_res = requests.get(uri)
-        oracle_res.raise_for_status()
+        loaded_bulk_json = json.load(bulk_json)
+        return loaded_bulk_json['download_uri']
+
+def update_oracle_json(oracle_uri: str) -> None:
+    with requests.get(oracle_uri,stream=True) as new_oracle_res:
         with open('oracle-cards.json','wb') as oracle_json:
-            oracle_json.write(oracle_res.content)
+            for chunk in new_oracle_res.iter_content(chunk_size=64):
+                oracle_json.write(chunk)
 
 def filter_oracle_json_for_(cube_list: list) -> json:
     ## recreate oracle card json for cube cards only
     filtered_cards = []
-    with open('oracle-cards.json','r', encoding='UTF-8') as file:
+    with open('oracle-cards.json','r',encoding='UTF-8') as file:
         cards = ijson.items(file,'item')
         for card in cards:
             if card.get('name') in cube_list:
@@ -47,7 +46,7 @@ def filter_oracle_json_for_(cube_list: list) -> json:
     return json.dumps(filtered_cards)
     
 def extract_info_from_(filtered_json: json) -> json:
-    ## extract name, mtgo id, and .png file from each card
+    ## extract id, name, and .png uri from each card
     extracted_json = []
     cards = ijson.items(filtered_json,'item')
     for card in cards:
